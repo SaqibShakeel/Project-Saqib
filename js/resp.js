cb360dbeed3b6779125df648eda752753469c548
SaqibShakeel <74822778+SaqibShakeel@users.noreply.github.com>
2020-11-21 09:03:14 -0500

    Add files via upload

---
+++ js/resp.js
@@ -0,0 +1,15 @@
+const burger = document.querySelector('.burger')
+const navbar = document.querySelector('.navbar')
+const navList = document.querySelector('.nav-list')
+const rightNav = document.querySelector('.rightnav')
+
+
+burger.addEventListener('.click', ()=>{
+    rightnav.classList.toggle('v-class-resp');
+    navlist.classList.toggle('v-class-resp');
+    navbar.classList.toggle('h-class-resp');
+
+    
+
+
+})
